3fbd37f385e3f6c4750451522a7bc3d5fbe0f52c
PalaboiCarti <cosmicvirgin6969@gmail.com>
2023-10-22 10:24:26 +0800

    Easier to read

--- ersatzBool.py
+++ ersatzBool.py
@@ -2,12 +2,13 @@
 #oct/22/2023 08:19
 import pandas as pd 
 
+# Data set.
 data = {
     'District': [1, 2, 3, 4, 5, 6, 7, 8, 9],
     'Color': ['blue', 'red', 'blue', 'red', 'blue', 'red', 'green', 'green', 'green']
 }
 
-
+# Put your functions here.
 def get_number():
     while True:
         try:
@@ -21,33 +22,44 @@ def get_number():
 
 user_number = get_number()
 
+# Data frame out of data set from earlier.
 color_map = pd.DataFrame(data)
 
-#Full List
-print("~~~~~~~~~~~~~~~~~~~~~~")
-print("FULL LIST")
-print(color_map)
+# Full List
+def full_list():
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    print("FULL LIST")
+    print(color_map)
 
-print("~~~~~~~~~~~~~~~~~~~~~~")
-print("BOOLEAN SERIES")
-
-#Checks if number is equal to any item in the list through a boolean
+# Checks if number is equal to any item in the list through a boolean.
 boolean_map = color_map['District'] == user_number
-print(boolean_map)
 
-print("~~~~~~~~~~~~~~~~~~~~~~")
-print("DISTRICT CHECKER")
+def boolean_series():
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    print("BOOLEAN SERIES")
+    print(boolean_map)
 
-#use the boolean map
+# Display corresponding values through the boolean map. 
+# Whatever number of an item in the dataframe corresponds to your inputted number, it will display the data of said item. 
+# Basta kaya mo na yan.
 district_info = color_map[boolean_map]
 district_color = district_info['Color'].values[0]
 district_number = district_info['District'].values[0]
 
+# Display that shit
 def display_info():
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    print("DISTRICT CHECKER")
     print(f"Your inputted number is : {user_number}")
     print(f"Your District color is : {district_color}")
 
-display_info()
-print("~~~~~~~~~~~~~~~~~~~~~~")
-print(f"Your inputted number \"{user_number}\" corresponds to District Number {district_number}")
-print(district_info)
+# Main function
+def main():
+    full_list()
+    boolean_series()
+    display_info()
+    print("~~~~~~~~~~~~~~~~~~~~~~")
+    print(f"Your inputted number \"{user_number}\" corresponds to District Number {district_number}")
+    print(district_info)
+
+main()
